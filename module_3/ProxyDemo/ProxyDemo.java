ff1dd6c8fcca32fd670c2cb282ceefa545b5d468
ArickCodeGuy <arickmail1997@gmail.com>
2026-04-23 08:42:45 +0300

    Proxy design pattern

--- module_3/ProxyDemo/ProxyDemo.java
+++ module_3/ProxyDemo/ProxyDemo.java
@@ -1,5 +1,35 @@
 package module_3.ProxyDemo;
 
 public class ProxyDemo {
+  public static void main(String[] args) {
+    Hawk hawk = new Hawk();
+    HawkProxy hawkProxy = new HawkProxy(hawk);
 
+    hawkProxy.fly();
+  }
+}
+
+interface Bird {
+  void fly();
+}
+
+class Hawk implements Bird {
+  @Override
+  public void fly() {
+    System.out.println("Hawk: fly");
+  }
+}
+
+class HawkProxy implements Bird {
+  private Hawk hawk;
+
+  HawkProxy(Hawk hawk) {
+    this.hawk = hawk;
+  }
+
+  @Override
+  public void fly() {
+    System.out.println("Proxy: fly");
+    hawk.fly();
+  }
 }
