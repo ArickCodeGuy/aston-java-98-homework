7d9b465cdc3f239877d40e81e97041fb349b069a
ArickCodeGuy <arickmail1997@gmail.com>
2026-04-28 10:55:13 +0300

    deadlock

--- module_4/DeadLock.java
+++ module_4/DeadLock.java
@@ -1,7 +1,49 @@
 package module_4;
 
-public class DeadLock {
-  public static void main(String[] args) {
+import java.util.ArrayList;
+import java.util.List;
 
+public class DeadLock {
+  static List<Integer> lock1 = new ArrayList<>();
+  static List<Integer> lock2 = new ArrayList<>();
+
+  public static void main(String[] args) {
+    Thread t1 = new Thread(() -> {
+      synchronized (lock1) {
+        System.out.println("Thread 1 locks lock1");
+        try {
+          Thread.sleep(100);
+        } catch (InterruptedException e) {
+        }
+        synchronized (lock2) {
+          System.out.println("Thread 1 locks lock2");
+        }
+      }
+    });
+
+    Thread t2 = new Thread(() -> {
+      synchronized (lock2) {
+        System.out.println("Thread 2 locks lock2");
+        try {
+          Thread.sleep(100);
+        } catch (InterruptedException e) {
+        }
+        synchronized (lock1) {
+          System.out.println("Thread 2 locks lock1");
+        }
+      }
+    });
+
+    t1.start();
+    t2.start();
+
+    try {
+      t1.join();
+      t2.join();
+    } catch (InterruptedException e) {
+    }
+
+    // Никогда не выполнится
+    System.out.println("Program finished");
   }
 }
